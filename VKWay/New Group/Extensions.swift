d0e8ee3578c8e06e92e7cc1f4fdc1524bf145733
Kefir4ek <kefir4ek@gmail.com>
2019-06-12 12:16:02 +0300

    1

--- VKWay/New Group/Extensions.swift
+++ VKWay/New Group/Extensions.swift
@@ -76,16 +76,5 @@ func needToUpdateTableData(name: String) -> Bool {
 }
 
 
-extension Results {
-    func toArray<T>(ofType: T.Type) -> [T] {
-        var array = [T]()
-        for i in 0 ..< count {
-            if let result = self[i] as? T {
-                array.append(result)
-            }
-        }
-        
-        return array
-    }
-}
+
 
